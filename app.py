112542e42314c266a5a3e68f69407e2d4984d516
momochong0 <momochong0@163.com>
2022-04-23 23:40:16 +0800

    添加首页

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask,escape,url_for
-app=Flask(__name__)
-@app.route('/')
-# @app.route('/home')
+from flask import Flask, escape, render_template, url_for,template_rendered
+
+app = Flask(__name__)
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+@app.route('/home')
 def hello():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
 
 @app.route('/user/<name>')
 def user_page(name):
     return 'hello %s' % escape(name)
+
 
 @app.route('/test')
 def test_url_for():
@@ -20,3 +38,9 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',name=name,movies=movies)
+    
